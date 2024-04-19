7a380426f8f2c244ade2e5c07f2b22458bca0637
david <=>
2024-04-19 12:02:39 +0200

    427 other: work in progress

--- cama_frontend/src/pages/Login/LoginPage.tsx
+++ cama_frontend/src/pages/Login/LoginPage.tsx
@@ -3,10 +3,16 @@
 import React from "react";
 import {Box, Button} from "@mui/material";
 import Logo  from "../../assets/images/orcid_logo_icon.png";
+import { loginUser } from "../../api/loginAPI";
 
 
 
 const LoginPage: React.FC = () => {
+    const handleLogin = async () => {
+        
+        const token = await loginUser();
+    }
+
     return (
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
             
@@ -21,7 +27,7 @@ const LoginPage: React.FC = () => {
             </Box>
             
             <Box display="flex" sx={{ justifyContent: "flex-start" }}>
-                <Button variant="contained" color="primary"  href="/login" sx={{ marginTop: 10, fontSize: "1.5rem" }}>
+                <Button variant="contained" color="primary"  href="/login" sx={{ marginTop: 10, fontSize: "1.5rem" }} onClick={() => {handleLogin()}}>
                     Login with ORCID
                     <img src={Logo} alt="ORCID logo" style={{ width: "20px", height: "20px", marginLeft: "5px" }} />
                 </Button>
