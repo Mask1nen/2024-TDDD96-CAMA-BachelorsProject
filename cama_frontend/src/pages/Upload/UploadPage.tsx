38bd2f540c358f063ae8b0b6954c06d5fa1ea2e0
Felix Claesson <75378259+felisk123@users.noreply.github.com>
2024-04-19 11:31:05 +0200

    460 fix: helped filip with stuff he cant do himself

--- cama_frontend/src/pages/Upload/UploadPage.tsx
+++ cama_frontend/src/pages/Upload/UploadPage.tsx
@@ -8,7 +8,23 @@ import Experimentform from "./experiment_form"
 import {AddCircleOutline} from "@mui/icons-material"
 import { v4 as uuidv4 } from 'uuid';
 
+import {Study, Experiment, Effect  } from  "../../api/newTypes"
+
 const UploadPage: React.FC = () => {
+
+	const [study, setStudy] = React.useState<Study>({
+		title: "",
+		authors: "",
+		abstract: "",
+		keywords: "",
+		category: "",
+		country: "",
+		year: 0,
+		doi: "",
+		peer_reviewed: false,
+		experiments: {}
+	});
+	
 
 	const [value, setValue] = React.useState(0);
 
@@ -17,7 +33,16 @@ const UploadPage: React.FC = () => {
 	  const addExperiment = () => {
 		let id = uuidv4();
 		setInputs(function(prev){
-			prev["experiments"][id] = {"experiment_id": id}
+			prev["experiments"][id] = {
+				"id": id,
+				"title": "",
+				"design": "",
+				"duration": "",
+				"frequency": "",
+				"intensity": "",
+				"grade": "",
+				"ni": "",
+			}
 			return prev;
 		}); 
 		setExperiments(prev => [...prev, id]);
