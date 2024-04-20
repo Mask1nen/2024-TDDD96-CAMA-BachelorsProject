1d0bfdbfdb943b4c41436eba1451a8528b9c218b
Tobias Berglind <tobias.berglind2001@gmail.com>
2024-04-20 13:32:24 +0200

    471 feat: serializers post operation finishes tests

--- cama_backend/myapi/serializers.py
+++ cama_backend/myapi/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from .models import Study, Country, Category, CamaUser,  StudyDesign, RiskOfBias, Grade, ParticipantDesign, Implementation, Experiment
-
+from .models import *
 
 
 class CountrySerializer(serializers.ModelSerializer):
@@ -35,6 +34,7 @@ class GradeSerializer(serializers.ModelSerializer):
         model = Grade
         fields = '__all__'  # Customize fields as needed
 
+
 # ParticipantDesign Serializer
 class ParticipantDesignSerializer(serializers.ModelSerializer):
     class Meta:
@@ -47,30 +47,74 @@ class ImplementationSerializer(serializers.ModelSerializer):
         model = Implementation
         fields = ['implementor']
 
+
+class EffectDataSerializer(serializers.ModelSerializer):
+    test_time = serializers.SlugRelatedField(slug_field='time', queryset=TestTime.objects.all())
+    effect_size_type = serializers.SlugRelatedField(slug_field='name', queryset=EffectSizeType.objects.all())
+    class Meta:
+        model = EffectData
+        fields = ["effect_size_type",
+                            "test_time",
+                            "test_name",
+
+                            "outcome",
+                            "outcome_full",
+                            "outcome_op",
+
+
+                            "gender_1",
+                            "gender_2",
+                            "gender_3",
+
+                            "d_var",
+                            "d",
+                            "f_stat",
+                            "t",
+                            "ri",
+                            "icc",
+
+                            "mean_age_1i",
+                            "mean_age_2i",
+
+                            "ai",
+                            "bi",
+                            "ci",
+                            "di",
+
+                            "sd1i",
+                            "sd2i",
+                            "n1i",
+                            "n2i",
+                            "m1i",
+                            "m2i",
+                            ]
+
+
 class ExperimentSerializer(serializers.ModelSerializer):
-    study_design = serializers.PrimaryKeyRelatedField(queryset=StudyDesign.objects.all())
+    grade = serializers.SlugRelatedField(slug_field='grade', queryset=Grade.objects.all())
+    study_design = serializers.SlugRelatedField(slug_field='design', queryset=StudyDesign.objects.all())
+    participant_design = serializers.SlugRelatedField(slug_field='design', queryset=ParticipantDesign.objects.all())
+    implemented = serializers.SlugRelatedField(slug_field='implementor', queryset=Implementation.objects.all())
+    risks=RiskOfBiasSerializer()
+    effect_datas = EffectDataSerializer(many=True)
 
     class Meta:
         model = Experiment
-        fields = '__all__'
-    # risks = RiskOfBiasSerializer()
-    # grade = serializers.PrimaryKeyRelatedField(queryset=Grade.objects.all())
-    # participant_design = serializers.PrimaryKeyRelatedField(queryset=ParticipantDesign.objects.all())
-    # implemented = serializers.PrimaryKeyRelatedField(queryset=Implementation.objects.all())
- 
-# Study Serializer
+        fields = ['grade', 'study_design', 'participant_design', 'implemented', 'intensity_n', 'ni', 'duration_week','frequency_n', 'intervention', 'intervention_op', 'target_population', 'mean_age', 'source', 'risks', 'effect_datas']
+
 class StudySerializer(serializers.ModelSerializer):
-    experiment = ExperimentSerializer(many=True)
+    experiments = ExperimentSerializer(many=True)
     country = serializers.PrimaryKeyRelatedField(queryset=Country.objects.all())
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 
     class Meta:
         model = Study
-        fields = '__all__'  # Customize fields as needed'''
+        fields = '__all__'  
+
 
     def create(self, validated_data):
         # Get experiment list
-        experiments_data = validated_data.pop('experiment')
+        experiments_data = validated_data.pop('experiments')
         
         
 
@@ -98,30 +142,38 @@ class StudySerializer(serializers.ModelSerializer):
         )
 
         # Create experiment tables
-        for entry in experiments_data:
+        for experiment_data in experiments_data:
             # Get all dictionaries from 
-            study_design_data = validated_data.pop('study_design')
-            # risks_data = validated_data.pop('risks')
-            # grade_data = validated_data.pop('grade')
+            #study_design_data = validated_data.pop('study_design')
+            risks_data = experiment_data.pop('risks')
+            #grade_data = entry.pop('grade')
             # participant_design_data = validated_data.pop('participant_design')
             # implemented_data = validated_data.pop('implemented')
 
-            study_design, _ = StudyDesign.objects.get_or_create(**study_design_data)
-            # risks, _ = RiskOfBias.objects.get_or_create(**risks_data)
-            # grade, _ = Grade.objects.get_or_create(**grade_data)
+          #  study_design, _ = StudyDesign.objects.get_or_create(**study_design_data)
+            risks, _ = RiskOfBias.objects.get_or_create(**risks_data)
+            # grade, _ = Grade.objects.get_or_create(grade_data)
             # participant_design, _ = ParticipantDesign.objects.get_or_create(**participant_design_data)
             # implemented, _ = Implementation.objects.get_or_create(**implemented_data)
 
+
+            effect_datas = experiment_data.pop('effect_datas')
+
             experiment = Experiment.objects.create(
-                # study_id=study,
+                #study_id=study,
                 # study_design=study_design,
-                # risks=risks,
+                risks=risks,
                 # grade=grade,
                 # participant_design=participant_design,
                 # implemented=implemented,
-                **entry
+                **experiment_data
             )  
+
+
+            for effect in effect_datas:
+                EffectData.objects.create(experiment_nr = experiment, **effect)
 
 
 
         return study
+
