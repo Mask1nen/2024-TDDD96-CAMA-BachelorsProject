f7e1c2cecb9b1ed62ab59a038e6f4d96f50a1925
david <=>
2024-04-20 23:31:17 +0200

    427 feat: login redirect now works

--- cama_frontend/src/pages/Login/LoginPage.tsx
+++ cama_frontend/src/pages/Login/LoginPage.tsx
@@ -1,17 +1,39 @@
 {/*This file conatins the code responible for controlling the content and function of the login page*/}
 
-import React from "react";
+import React, { useState } from "react";
 import {Box, Button} from "@mui/material";
 import Logo  from "../../assets/images/orcid_logo_icon.png";
 import { loginUser } from "../../api/loginAPI";
+const loginUrl = "https://orcid.org/oauth/authorize?client_id=APP-IZWWE416AT5JC4N6&response_type=token&scope=openid&redirect_uri=http://192.168.0.34:3000/Login"
 
 
 
 const LoginPage: React.FC = () => {
-    const handleLogin = async () => {
-        
-        const token = await loginUser();
-    }
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        window.location.href = loginUrl;
+    };
+
+    // Function to extract token from URL after ORCID callback
+    const extractTokenFromURL = () => {
+        alert("extractTokenFromURL");
+        const urlParams = new URLSearchParams(window.location.search);
+        const access_token = urlParams.get('access_token');
+        const id_token = urlParams.get('id_token');
+        const tokenId = urlParams.get('tokenId');
+        alert(access_token);
+        if (access_token) {
+            // Handle the code (token) received from ORCID
+            console.log('Received code from ORCID:', access_token);
+            setIsLoggedIn(true);
+        }
+    };
+
+    // Check for token in URL on component mount
+    React.useEffect(() => {
+        extractTokenFromURL();
+    }, []);
 
     return (
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
@@ -27,7 +49,7 @@ const LoginPage: React.FC = () => {
             </Box>
             
             <Box display="flex" sx={{ justifyContent: "flex-start" }}>
-                <Button variant="contained" color="primary"  href="/login" sx={{ marginTop: 10, fontSize: "1.5rem" }} onClick={() => {handleLogin()}}>
+                <Button variant="contained" color="primary"  sx={{ marginTop: 10, fontSize: "1.5rem" }} onClick={() => {handleLogin()}}>
                     Login with ORCID
                     <img src={Logo} alt="ORCID logo" style={{ width: "20px", height: "20px", marginLeft: "5px" }} />
                 </Button>
