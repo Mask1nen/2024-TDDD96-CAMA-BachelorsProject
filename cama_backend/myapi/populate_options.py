75183642df8ec0dd5c16f2bec56ffd30195265a1
Jonas Fransson <jonfr872@student.liu.se>
2024-04-24 12:19:10 +0200

    450 feat: Added population feature to the database, all option tables now gets populated at migration

--- cama_backend/myapi/populate_options.py
+++ cama_backend/myapi/populate_options.py
@@ -4,6 +4,19 @@ from myapi.experiment import StudyDesign, RiskOfBias, ParticipantDesign, Impleme
 from myapi.effect_data import TestTime, EffectSizeType
 from django.core.exceptions import *
  
+""" To populate options tabels add the following lines to the end of the operations list in 
+class Migration(migrations.Migration): 
+in the migration file 
+
+migrations.RunPython(
+            code=populate_option_tables,
+            reverse_code=migrations.RunPython.noop
+            ),
+            
+and add the 
+from myapi.populate_options import populate_option_tables
+import at the top
+"""
 
 def populate_country():
     european_countries = [
@@ -54,71 +67,55 @@ def populate_country():
     "United Kingdom",
     "Vatican City"]
     for country in european_countries:
-        try:
-            Country.objects.get(name=country)
-        except ObjectDoesNotExist:
-            Country(name=country).save()
+        count = Country(name=country)
+        count.save()
             
     
 def populate_category():
     options = ['Language', 'STEM', 'Math', 'Other']
     for option in options:
-        try:
-            Category.objects.get(name=option)
-        except ObjectDoesNotExist:
-            Category(name=option).save()
+        category = Category(name=option)
+        category.save()
 
 def populate_study_design():
     options = ['RCT', 'QES']
     for option in options:
-        try:
-            StudyDesign.objects.get(design=option)
-        except ObjectDoesNotExist:
-            StudyDesign(design=option).save()
+        study_design = StudyDesign(design=option)
+        study_design.save()
 
 def populate_risk_of_bias():
     options = ['low', 'moderate', 'high', 'N/A']
     for option in options:
-        try:
-            RiskOfBias.objects.get(rob=option)
-        except ObjectDoesNotExist:
-            RiskOfBias(rob=option).save()
+        risk_of_bias = RiskOfBias(rob=option)
+        risk_of_bias.save()
 
 def populate_participant_design():
     options = ['within', 'between', 'mixed']
     for option in options:
-        try:
-            ParticipantDesign.objects.get(design=option)
-        except ObjectDoesNotExist:
-            ParticipantDesign(design=option).save()
+        participant_design = ParticipantDesign(design=option)
+        participant_design.save()
 
 def populate_implementation():
     options = ['researcher', 'teacher', 'paraprofessional']
     for option in options:
-        try:
-            Implementation.objects.get(implementor=option)
-        except ObjectDoesNotExist:
-            Implementation(implementor=option).save()
+        implementation = Implementation(implementor=option)
+        implementation.save()
 
 def populate_test_time():
     options = ['baseline(pre-test)', 'post-test', 'follow-up']
     for option in options:
-        try:
-            TestTime.objects.get(time=option)
-        except ObjectDoesNotExist:
-            TestTime(time=option).save()
+        test_time = TestTime(time=option)
+        test_time.save()
 
 def populate_effect_size_type():
     options = ['SMD', 'RR/OR']
     for option in options:
-        try:
-            EffectSizeType.objects.get(type=option)
-        except ObjectDoesNotExist:
-            EffectSizeType(type=option).save()
+        effect_size_type = EffectSizeType(name=option)
+        effect_size_type.save()
             
-def populate_option_tables():
+def populate_option_tables(apps, schema_editor):
     populate_category()
-    populate_category()
+    populate_country()
     populate_risk_of_bias()
     populate_study_design()
     populate_participant_design()
@@ -141,4 +138,3 @@ def delete_all_options():
     delete_table_options(TestTime)
     delete_table_options(EffectSizeType)
     
-populate_option_tables()
